765836767e2f68a8fa3f695684b9083ea370076a
Jason Burke <jpburke73@gmail.com>
2024-03-05 18:37:45 -0500

    first version of shadowdark system

---
+++ src/systems/shadowdark.js
@@ -0,0 +1,18 @@
+export default {
+    "VERSION": "1.0.0",
+
+    "ACTOR_CLASS_TYPE": "character",
+
+    "ITEM_QUANTITY_ATTRIBUTE": "system.quantity",
+
+    "ITEM_PRICE_ATTRIBUTE": "system.cost",
+
+	"ITEM_FILTERS": [
+		{
+			"path": "type",
+			"filters": "script,Spell"
+        }
+    ],
+
+    "ITEM_SIMILARITIES": [ "name", "type" ]
+}
