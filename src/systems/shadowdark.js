3a4d409709eec2f4775d2c1d0f90cadf6697a28a
Jason Burke <jpburke73@gmail.com>
2024-03-06 06:20:25 -0500

    formatted shadowdark.js in dnd5e style

--- src/systems/shadowdark.js
+++ src/systems/shadowdark.js
@@ -1,18 +1,24 @@
 export default {
-    "VERSION": "1.0.0",
 
-    "ACTOR_CLASS_TYPE": "Player",
+  "VERSION": "1.0.0",
 
-    "ITEM_QUANTITY_ATTRIBUTE": "system.quantity",
+  // The actor class type is the type of actor that will be used for the default item pile actor that is created on first item drop.
+  "ACTOR_CLASS_TYPE": "Player",
 
-    "ITEM_PRICE_ATTRIBUTE": "system.cost",
+  // The item quantity attribute is the path to the attribute on items that denote how many of that item that exists
+  "ITEM_QUANTITY_ATTRIBUTE": "system.quantity",
 
-	"ITEM_FILTERS": [
-		{
-			"path": "type",
-			"filters": "script,Spell"
-        }
-    ],
+  // The item price attribute is the path to the attribute on each item that determine how much it costs
+  "ITEM_PRICE_ATTRIBUTE": "system.cost",
 
-    "ITEM_SIMILARITIES": [ "name", "type" ]
+  // Item filters actively remove items from the item pile inventory UI that users cannot loot, such as spells, feats, and classes
+  "ITEM_FILTERS": [
+    {
+      "path": "type",
+      "filters": "script,Spell"
+    },
+  ],
+
+  // Item similarities determines how item piles detect similarities and differences in the system
+  "ITEM_SIMILARITIES": ["name", "type"],
 }
