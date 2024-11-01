ec386c9b5058b7a00b3d3fa2a4e6e28923671e44
Matthew B Stroud <mbstroud@gmail.com>
2024-11-01 08:52:29 -0400

    remove coalesce

--- src/API/private-api.js
+++ src/API/private-api.js
@@ -292,7 +292,7 @@ export default class PrivateAPI {
 				handler({ item, items: newItems });
 			}
 			const uniqueIds = new Set(items.map(i => i.id ?? i._id));
-			items = items.concat(newItems.filter(i => !uniqueIds.has(i._id ?? i.id)).map(item => ({
+			items = items.concat(newItems.filter(i => !uniqueIds.has(i._id)).map(item => ({
 				_id: item._id,
 				flags: undefined,
 				quantity: Utilities.getItemQuantity(item)
